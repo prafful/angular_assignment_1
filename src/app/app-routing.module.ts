7f54e580aea90ca58ee41fac9da0e6b7fc3001a5
Prafful Daga <prraful@gmail.com>
2019-07-19 12:55:42 +0530

    assignmetn 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShowproductComponent } from './shop/showproduct/showproduct.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'showproducts/:categoryname',
+    component:ShowproductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop/shop.component';
+import { HeaderComponent } from './shop/header/header.component';
+import { CategoryComponent } from './shop/category/category.component';
+import { ShowproductComponent } from './shop/showproduct/showproduct.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    HeaderComponent,
+    CategoryComponent,
+    ShowproductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
